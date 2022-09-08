0231f6151bed481084a176d2cfed90ba9dfddf3d
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-08 10:35:57 +0100

    Updates

---
+++ precache-manifest.3d8f2f390561249c0f59ea0858f6f47a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c039f0e3171db667e4fd",
+    "url": "/tic-tac-toe/static/css/main.fd8adf30.chunk.css"
+  },
+  {
+    "revision": "c039f0e3171db667e4fd",
+    "url": "/tic-tac-toe/static/js/main.c039f0e3.chunk.js"
+  },
+  {
+    "revision": "fd0cfea648d4e488b62d",
+    "url": "/tic-tac-toe/static/js/1.fd0cfea6.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "8ca6b504cb58819edf25dc933126adaa",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
